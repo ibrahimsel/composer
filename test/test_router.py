40378bd28dc1baf1d2b01b0e4a90e6d085a6006f
Samet Karabulut <samet.karabulut@eteration.com>
2024-09-30 10:05:37 +0300

    added test_router

---
+++ test/test_router.py
@@ -0,0 +1,39 @@
+import unittest
+from unittest.mock import MagicMock, patch
+import rclpy
+from composer.router import Router
+
+
+class TestRouter(unittest.TestCase):
+
+    def setUp(self):
+        self.pipelines = {
+            'start': MagicMock(),
+            'kill': MagicMock(),
+            'apply': MagicMock()
+        }
+        self.payload = {'stackId': 'org.eclipse.muto.sandbox:test', 'action': 'kill'}
+        self.pipeline = self.pipelines.get('kill')
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        rclpy.init()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        rclpy.shutdown()
+
+    def test_route(self):
+        main_route = Router(self.pipelines)
+        main_route.route(self.payload.get('action', ''))
+        self.pipeline.execute_pipeline.assert_called_once()
+        
+    def test_route_no_pipeline_found(self):
+        main_route = Router(self.pipelines)
+        with patch('builtins.print') as mock_print:
+            main_route.route('no_action')
+            mock_print.assert_called_with("No pipeline found for action: no_action")
+
+
+if __name__ == "__main__":
+    unittest.main()
